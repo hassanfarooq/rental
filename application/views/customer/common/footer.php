cf958cfbd45d04238d6a1a70531fdf27220fd769
hassanfarooq <hassanfarooq09@gmail.com>
2016-11-01 13:16:17 +0500

    update header, footer, sidebar

--- application/views/customer/common/footer.php
+++ application/views/customer/common/footer.php
@@ -1,17 +1,18 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2016-2017 <a href="#">Car Rental System</a>.</strong> All rights
     reserved.
   </footer>
 <!-- jQuery 2.2.3 -->
 <script src="<?php echo base_url('assets/customer/plugins/jQuery/jquery-2.2.3.min.js'); ?>"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<script src="<?php echo base_url('assets/customer/plugins/jQuery/jquery-ui.min.js'); ?>"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.6 -->
 <script src="<?php echo base_url('assets/customer/js/bootstrap.min.js');?> "></script>
+<script src="<?php echo base_url('assets/customer/plugins/daterangepicker/moment.min.js');?>"></script>
 <script src="<?php echo base_url('assets/customer/plugins/daterangepicker/daterangepicker.js');?>"></script>
 <script src="<?php echo base_url('assets/customer/plugins/datepicker/bootstrap-datepicker.js');?>"></script>
 <script src="<?php echo base_url('assets/customer/js/app.min.js');?>"></script>
--- application/views/customer/common/sidebar.php
+++ application/views/customer/common/sidebar.php
@@ -4,10 +4,10 @@
     <!-- Sidebar user panel -->
     <div class="user-panel">
       <div class="pull-left image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        <img src="<?php echo base_url('assets/customer/img/profile-default.jpg'); ?>" class="img-circle" alt="User Image">
       </div>
       <div class="pull-left info">
-        <p>Alexander Pierce</p>
+        <p>Hi, <?php echo ucwords(get_logindata('name')); ?></p>
         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
       </div>
     </div>
